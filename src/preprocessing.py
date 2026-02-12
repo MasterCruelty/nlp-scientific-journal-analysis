58c8b3528972dd5180d2ae0f99b74c29feb9aa11
MasterCruelty <robertino.antoniello@gmail.com>
2026-02-12 23:45:30 +0100

    add docs

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,1 +1,7 @@
-#WIP
+
+
+"""
+    preprocessing.py help us to preprocess the dataset we obtained by apply lower case to any word,
+    by removing puntuaction or any symbol that doesn't give us useful information.
+    Stop words will be removed too.
+"""
