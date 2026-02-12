ec0b21ac128495d1116c969171473ae442702bcf
MasterCruelty <robertino.antoniello@gmail.com>
2026-02-12 21:07:30 +0100

    refactoring get-dataset.py

--- dataset/get-dataset.py
+++ dataset/get-dataset.py
@@ -4,20 +4,6 @@ import time
 import re
 import html
 
-#issn number of the journal (Frontiers in Artificial Intelligence)
-issn = "26248212"   
-
-#range of years
-start_date = "2021-01-01"
-end_date = "2025-12-31"
-
-#api url for the get call
-url = f"https://api.crossref.org/journals/{issn}/works"
-
-#number of rows; initializing offset for pages; initializing array of all papers we will get.
-rows = 1000
-offset = 0
-all_papers = []
 
 #function which clean the raw html abstract format
 def clean_abstract(raw):
@@ -27,45 +13,70 @@ def clean_abstract(raw):
     text = html.unescape(text)
     return text.strip()
 
-#fetching articles increasing offset page by page until we don't get any articles
-while True:
-    params = {
-        "filter": f"from-pub-date:{start_date},until-pub-date:{end_date}",
-        "rows": rows,
-        "offset": offset
-    }
+def fetch_articles(start_date,end_date,rows,url):
+    offset = 0
+    all_papers = []
+    #fetching articles increasing offset page by page until we don't get any articles
+    while True:
+        params = {
+            "filter": f"from-pub-date:{start_date},until-pub-date:{end_date}",
+            "rows": rows,
+            "offset": offset
+        }
 
-    resp = requests.get(url, params=params)
-    data = resp.json()
+        resp = requests.get(url, params=params)
+        data = resp.json()
 
-    #no items -> we already finished -> break
-    items = data["message"]["items"]
-    if not items:
-        break
+        #no items -> we already finished -> break
+        items = data["message"]["items"]
+        if not items:
+            break
 
-    #for each item obtained, we get a few fields of interest.
-    for item in items:
-        title = item.get("title", [""])[0]
-        abstract_raw = item.get("abstract")
-        #without any abstract the item is useless, otherwise we clean it from html tags
-        if not abstract_raw:
-            continue
-        abstract_cleaned = clean_abstract(abstract_raw)
-        
-        year = item.get("issued", {}).get("date-parts", [[None]])[0][0]
-        doi = item.get("DOI")
+        #for each item obtained, we get a few fields of interest.
+        for item in items:
+            title = item.get("title", [""])[0]
+            abstract_raw = item.get("abstract")
+            #without any abstract the item is useless, otherwise we clean it from html tags
+            if not abstract_raw:
+                continue
+            abstract_cleaned = clean_abstract(abstract_raw)
+            
+            year = item.get("issued", {}).get("date-parts", [[None]])[0][0]
+            doi = item.get("DOI")
 
-        all_papers.append({
-            "title": title,
-            "abstract": abstract_cleaned,
-            "year": year,
-            "doi": doi
-        })
+            all_papers.append({
+                "title": title,
+                "abstract": abstract_cleaned,
+                "year": year,
+                "doi": doi
+            })
 
-    offset += rows
-    time.sleep(1) 
+        offset += rows
+        time.sleep(1)
+    return all_papers 
 
-df = pd.DataFrame(all_papers)
-df.to_csv("frontiers_ai_2021_2025.csv", index=False)
+#build dataset given the articles extracted by api call
+def build_dataset(articles):
+    df = pd.DataFrame(articles)
+    df.to_csv("dataset.csv", index=False)
+    print("Total articles:", len(df))
 
-print("Total articles:", len(df))
+
+##############################
+# execution 
+##############################
+
+#issn number of the journal (Frontiers in Artificial Intelligence)
+issn = "26248212"   
+url = f"https://api.crossref.org/journals/{issn}/works"
+
+#range of years
+start_date = "2021-01-01"
+end_date = "2025-12-31"
+
+#number of rows
+rows = 1000
+
+articles = fetch_articles(start_date,end_date,rows,url)
+
+build_dataset(articles)
