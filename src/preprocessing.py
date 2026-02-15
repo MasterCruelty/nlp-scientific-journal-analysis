dfcd994ce1b5ee33dcc429db24da48aa4ce88935
MasterCruelty <robertino.antoniello@gmail.com>
2026-02-15 14:05:34 +0100

    preprocessing text

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,3 +1,9 @@
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+#define set of stop words in the english language
+STOPWORDS = set(stopwords.words("english"))
 
 
 """
@@ -5,3 +11,18 @@
     by removing puntuaction or any symbol that doesn't give us useful information.
     Stop words will be removed too.
 """
+
+
+def preprocess_text(text):
+    text = text.lower()
+
+    #remove puntuactions and any other not meaningful words.
+    text = re.sub(r"[^a-z\s]", " ", text)
+
+    # tokenization by white space
+    tokens = text.split()
+
+    # remove all stopwords defined 
+    tokens = [t for t in tokens if t not in STOPWORDS]
+
+    return " ".join(tokens)
